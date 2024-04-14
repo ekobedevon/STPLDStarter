f8810eff26cf2a888e4387cf8dd3e7f730be9ced
Kobe Edmond <e.kobedevon@gmail.com>
2024-04-14 06:35:04 +0000

    add fix to login

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -24,7 +24,7 @@ export const actions: Actions = {
 			typeof username !== 'string' ||
 			username.length < 3 ||
 			username.length > 31 ||
-			!/^[a-z0-9_-]+$/.test(username)
+			!/^[a-z0-9_-]+$/.test(username.toLocaleLowerCase())
 		) {
 			return fail(400, {
 				message: 'Invalid username'
